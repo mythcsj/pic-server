bafca1fe41f4dfe7a5f421cceacb97d762343260
mythcsj <mythcsj@163.com>
2023-10-05 20:29:47 +0800

    [fix] modify code

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,17 @@
-import 'dotenv/config';
 import type { VercelRequest, VercelResponse } from '@vercel/node';
+import axios from 'axios';
+import 'dotenv/config';
 
-export default (request: VercelRequest, response: VercelResponse) => {
-    response.status(200).send(JSON.stringify(request.query))
+const { API_ENDPOINT: url, API_TOKEN: token } = process.env
+
+export default async (req: VercelRequest, res: VercelResponse) => {
+    const { style = '国风 宫崎骏', desc = '繁华 星空 夜景 梦幻 都市 街道' } = req.query
+
+    const { data: { data: { pictures } } } = await axios.post(`${url}`, { style, desc }, { headers: { 'token': token } })
+
+    const picUrls = Object.values(pictures) as string[];
+
+    return res.status(200).json({
+        picUrls
+    })
 }
